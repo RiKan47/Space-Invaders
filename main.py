c82983345911f513a3ad348566d2586bdbd76c6d
RiKan47 <rishikanth47@gmail.com>
2020-11-01 09:39:34 +0530

    added enemy and game over notification

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialising the pygame window
 pygame.init()
@@ -12,7 +13,7 @@ pygame.display.set_icon(icon)
 
 # player
 player_img = pygame.image.load("rocket_player.png")
-player_x = 370
+player_x = 736
 player_y = 480
 player_change = 5
 
@@ -22,13 +23,37 @@ def player(x, y):
     screen.blit(player_img, (x, y))
 
 
+# enemy
+enemy_img = pygame.image.load("monster_enemy.png")
+enemy_x = random.randint(5, 731)
+enemy_y = random.randint(10, 200)
+# enemy_y = 396
+enemy_change = .15
+
+
+def enemy(x, y):
+    screen.blit(enemy_img, (x, y))
+
+
+# text on screen
+font = pygame.font.Font('freesansbold.ttf', 32)
+font_score = pygame.font.Font('freesansbold.ttf', 20)
+
+
+def show_score(x, y):
+    score = font_score.render("Score : ", True, (128, 0, 0))
+    screen.blit(score, (x, y))
+
+
 # game loop
-pygame.key.set_repeat(1, 25)
+pygame.key.set_repeat(1, 15)
 running = True
 while running:
     # for background
     screen.fill((0, 128, 0))
     player(player_x, player_y)
+    enemy(enemy_x, enemy_y)
+    show_score(10, 10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,5 +70,11 @@ while running:
                 if player_x > 731:
                     player_x = 731
                 player(player_x, player_y)
-
+    enemy_x = enemy_x + enemy_change
+    if enemy_x >= 731 or enemy_x <= 5:
+        enemy_change = -enemy_change
+        enemy_y += 20
+    if enemy_y >= 416:
+        game_over = font.render("Game Over! LOL", True, (255, 0, 0))
+        screen.blit(game_over, (300, 300))
     pygame.display.update()
